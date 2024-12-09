3f07f3897116ac0d2f85c5fa2ebbca9337664ebd
Paloma Mtnz Andrada <palomandrada@Palomas-MBP.localdomain>
2024-12-09 20:31:57 +0100

    Productly nav

---
+++ Lesson_22/main.dev.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// CamelCaseFileNames are bettter
+//Create & work with data 
+//create & work with html elements 
+//connect to the remote data (server) 
+//Name -> Paloma 
+var myName = 'Paloma'; //Not able to resign it. To do so, use 'let'
+//How to declare a viariable: 
+
+var userName1 = 'Paloma1';
+userName1 = "Paloma2"; //
+
+var userName2 = confirm('How are you?');
+console.log(userName2); //
+
+var num1 = +prompt('Add the frist number'); //+prompt tansform any property into a number 
+
+var num2 = Number(prompt('Add the second number'));
+var num3 = parseInt(prompt('Add the third number')); //will conver string by character by character
+
+console.log("Result of adition ".concat(num1, " and ").concat(num2, " will be ").concat(result));
+var transformedName = myName.toUpperCase(); //myName can be chosen by us and can be anything. CamelCase recommended
+// const a = 1; 
+// const b = 2; 
+// console.log(a + b);
+//1 
+//2
