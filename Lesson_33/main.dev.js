549a034afe499d4d4254f8937135d2e050ffb3e1
Paloma Mtnz Andrada <palomandrada@Palomas-MBP.localdomain>
2025-01-16 19:09:02 +0100

    Weather app

--- Lesson_33/main.dev.js
+++ Lesson_33/main.dev.js
@@ -1,59 +1,39 @@
 "use strict";
 
-// const API = "03c72cb266d9e5916be3fbd03af22589"
-// function fetchWeather (cityName) {
-//     const URL = `http://api.openweathermap.org/data/2.5/find?q=${cityName}&appid=${API}&units=metric`;
-//     fetch (URL)
-//     .then (res => res.json())
-//     .then (response => {
-//         const weather = response.list[0]?.main;
-//         renderWeatherData (weather);
-//     })
-// }
-// function searchWeather() {
-//     const cityName = document.querySelector(".city").value || "Paris";
-//     fetchWeather(cityName + temperature);
-// }
-// function renderWeatherData(data){
-//     const temp_wrapper = document.querySelector(".temperature")
-//     temp_wrapper.textContent = `Current temperature: ${Math.round(data.temp)}℃`;
-//     const name_wrapper = document.querySelector(".name")
-//     name_wrapper = data.name;
-// }
 var API = "03c72cb266d9e5916be3fbd03af22589";
-var URL = "http://api.openweathermap.org/data/2.5/weather?units?=metric&q=";
-var searchBox = document.querySelector(".search input");
-var searchBtn = document.querySelector(".search button");
 
-function checkWeather(city) {
-  var response, data;
-  return regeneratorRuntime.async(function checkWeather$(_context) {
-    while (1) {
-      switch (_context.prev = _context.next) {
-        case 0:
-          _context.next = 2;
-          return regeneratorRuntime.awrap(fetch(URL + city + "&appid=".concat(API)));
-
-        case 2:
-          response = _context.sent;
-          _context.next = 5;
-          return regeneratorRuntime.awrap(response.json());
-
-        case 5:
-          data = _context.sent;
-          console.log(data);
-          document.querySelector(".city").innerHTML = data.name;
-          document.querySelector(".temp").innerHTML = Math.round(main.data.temp) + "°C";
-
-        case 9:
-        case "end":
-          return _context.stop();
-      }
-    }
+function fetchWeather(cityName) {
+  var URL = "http://api.openweathermap.org/data/2.5/find?q=".concat(cityName, "&appid=").concat(API, "&units=metric");
+  fetch(URL).then(function (res) {
+    return res.json();
+  }).then(function (response) {
+    var weather = response.list[0];
+    renderWeatherData(weather);
   });
 }
 
-searchBtn.addEventListener("click", function () {
-  checkWeather(searchBox.value);
-});
-checkWeather();
+function searchWeather() {
+  var cityName = document.querySelector(".city").value || "Paris";
+  fetchWeather(cityName);
+}
+
+function renderWeatherData(data) {
+  var temp_wrapper = document.querySelector(".temperature");
+  temp_wrapper.textContent = "Current temperature: ".concat(Math.round(data.main.temp), "\u2103");
+  var name_wrapper = document.querySelector(".cityName");
+  name_wrapper.textContent = "City: ".concat(data.name);
+} // const API = "03c72cb266d9e5916be3fbd03af22589"
+// const URL = "http://api.openweathermap.org/data/2.5/weather?units?=metric&q=";
+// const searchBox = document.querySelector(".search input");
+// const searchBtn = document.querySelector(".search button");
+// async function checkWeather (city) {
+//     const response = await fetch(URL + city + `&appid=${API}`);
+//     var data = await response.json();
+//     console.log (data);
+//     document.querySelector(".city").innerHTML = data.name;
+//     document.querySelector(".temp").innerHTML = Math.round(main.data.temp) + "°C";
+// }
+// searchBtn.addEventListener("click", ()=> {
+//     checkWeather(searchBox.value);
+// })
+// checkWeather ();
